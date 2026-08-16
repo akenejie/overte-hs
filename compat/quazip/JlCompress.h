fca123b9449b2f53a63d4cf48df7ca5a8c73de10
akenejie <akenegit@outlook.com>
2026-08-16 18:35:14 +0900

    WebUIを消去

--- compat/quazip/JlCompress.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// Stub for headless build - quazip not available
-#include <QString>
-#include <QStringList>
-class JlCompress {
-public:
-    static bool compressDir(const QString& fileCompressed, const QString& dir, bool includeRoot = true) { (void)fileCompressed; (void)dir; (void)includeRoot; return false; }
-    static QStringList extractDir(const QString& fileCompressed, const QString& dir) { (void)fileCompressed; (void)dir; return {}; }
-};
--- compat/quazip/quazipfileinfo.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-// Stub for headless build - quazip not available
-#include "quazip.h"
-#include "quazipdir.h"
