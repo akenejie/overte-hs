1c3b593b4f30af03f13606cdeae7c8275f76c16f
akenejie <akenegit@outlook.com>
2026-08-20 20:17:02 +0900

    debug: add CRASH-DBG logging to assignment creation path
    Add fprintf(stderr) + fflush tracing at every step of the assignment
    creation path to identify the exact location of the ACCESS_VIOLATION
    crash in assignment-client child processes on Windows.

--- assignment-client/src/AssignmentFactory.cpp
+++ assignment-client/src/AssignmentFactory.cpp
@@ -11,6 +11,7 @@
 
 #include "AssignmentFactory.h"
 
+#include <cstdio>
 #include <udt/PacketHeaders.h>
 
 #include "Agent.h"
@@ -25,27 +26,39 @@ ThreadedAssignment* AssignmentFactory::unpackAssignment(ReceivedMessage& message
 
     quint8 packedType;
     if (message.peekPrimitive(&packedType) != sizeof(packedType)) {
+        fprintf(stderr, "[CRASH-DBG] unpackAssignment: peekPrimitive failed\n");
+        fflush(stderr);
         return nullptr;
     }
 
     Assignment::Type unpackedType = (Assignment::Type) packedType;
+    fprintf(stderr, "[CRASH-DBG] unpackAssignment: type=%d\n", (int)unpackedType);
+    fflush(stderr);
 
     switch (unpackedType) {
         case Assignment::AudioMixerType:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: new AudioMixer\n"); fflush(stderr);
             return new AudioMixer(message);
         case Assignment::AvatarMixerType:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: new AvatarMixer\n"); fflush(stderr);
             return new AvatarMixer(message);
         case Assignment::AgentType:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: new Agent\n"); fflush(stderr);
             return new Agent(message);
         case Assignment::EntityServerType:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: new EntityServer\n"); fflush(stderr);
             return new EntityServer(message);
         case Assignment::AssetServerType:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: new AssetServer\n"); fflush(stderr);
             return new AssetServer(message);
         case Assignment::MessagesMixerType:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: new MessagesMixer\n"); fflush(stderr);
             return new MessagesMixer(message);
         case Assignment::EntityScriptServerType:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: new EntityScriptServer\n"); fflush(stderr);
             return new EntityScriptServer(message);
         default:
+            fprintf(stderr, "[CRASH-DBG] unpackAssignment: unknown type %d\n", (int)unpackedType); fflush(stderr);
             return nullptr;
     }
 }
